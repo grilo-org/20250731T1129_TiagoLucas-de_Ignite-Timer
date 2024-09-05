351f8042231ac1c0a20e20a41ac004f2a3f024c4
Tiago Lucas Da Silva <titi020604@gmail.com>
2024-09-05 18:28:31 -0300

    History

--- 02-ignite-timer/src/styles/global.ts
+++ 02-ignite-timer/src/styles/global.ts
@@ -16,7 +16,6 @@ export const GlobalStyle = createGlobalStyle`
 body {
   background: ${(props) => props.theme["gray-900"]};
   color: ${(props) => props.theme["gray-300"]};
-  color: #fff;
 
   -webkit-font-smoothing: antialiased;
 }
